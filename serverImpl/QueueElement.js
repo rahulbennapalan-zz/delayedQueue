c068296bcf10c8233769144c5aa12e002e699198
Bennapalan Rahul <innoz@innoztech.(none)>
2013-10-25 18:21:49 +0530

    new impl

--- serverImpl/QueueElement.js
+++ serverImpl/QueueElement.js
@@ -23,23 +23,5 @@ QueueElement.prototype.getHost = function() {
 	return this.host;
 };
 
-
-/*
-
-// enqueue
-Queue.prototype.enqueue = function(attr) {
-	this.queue.push(attr);
-};
-
-// dequeue
-Queue.prototype.dequeue = function() {
-	return this.queue.pop();
-};
-
-// dequeue all
-Queue.prototype.dequeueAll = function() {
-	this.queue = [];
-};
-*/
 // export the class
 module.exports = QueueElement;
