e30203450e7a8c5f5fbefbe165ec3ca31172d054
Bennapalan Rahul <innoz@innoztech.(none)>
2013-10-25 18:25:40 +0530

    new impl

--- test.js
+++ test.js
@@ -5,17 +5,11 @@ var onResponse = function(response) {
 	console.log(response);
 };
 
-Queue.enqueue("1", function(res) {
-	console.log(res);
-}, new Date().getTime() + 10000);
+Queue.enqueue("1", onResponse, new Date().getTime() + 10000);
 
-Queue.enqueue("2", function(res) {
-	console.log(res);
-}, new Date().getTime() + 12000);
+Queue.enqueue("2", onResponse, new Date().getTime() + 12000);
 
-Queue.enqueue("3", function(res) {
-	console.log(res);
-}, new Date().getTime() + 10000);
+Queue.enqueue("3", onResponse, new Date().getTime() + 10000);
 
 Queue.startProcessing(onResponse, 1000, true);
 
